ece53917adc90023c34d1badab188387edd5b239
HirunaD <de_silva_pkhd_e22@engug.ruh.ac.lk>
2024-12-22 15:20:36 +0530

    Reimplement missing components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Calender from '@/section/Home/Calender'
 
 const Home = () => {
   return (
-    <div className='p-20'>
+    <div className='max-w-[1920px] mx-auto p-20'>
       <Calender />
     </div>
   )
