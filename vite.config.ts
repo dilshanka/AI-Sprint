4b2f2a0c1f82895988cce5809738f829b07390fe
dilshanka <ranaweera_kd_e22@engug.ruh.ac.lk>
2024-12-22 15:27:20 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/ 
 export default defineConfig({ 
   plugins: [react()], 
+  base: "https://dilshanka.github.io/AI-Sprint/",
   resolve: { 
     alias: { 
       "@": path.resolve(__dirname, "src"), 
